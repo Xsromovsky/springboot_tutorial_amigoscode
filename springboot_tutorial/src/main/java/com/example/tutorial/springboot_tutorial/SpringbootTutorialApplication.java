9fa48a6e71dfaf52c0353986d0e17388d37fd6e0
Samuel_Sromovsky_111151 <99825651+Xsromovsky@users.noreply.github.com>
2022-04-26 23:22:04 +0200

    Add files via upload

---
+++ springboot_tutorial/src/main/java/com/example/tutorial/springboot_tutorial/SpringbootTutorialApplication.java
@@ -0,0 +1,16 @@
+package com.example.tutorial.springboot_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTutorialApplication.class, args);
+    }
+
+
+
+
+}
